8f13b8e2bb68b32ba27658287ca8c72a14a86ccf
AndresPerez07 <56890923+AndresPerez07@users.noreply.github.com>
2019-11-26 13:38:10 -0500

    Add files via upload

--- stady-state.cpp
+++ stady-state.cpp
@@ -10,13 +10,13 @@ void escribe(double *v, double t, int N);
 void copia(double *a, double *b, int N);
 double maximo(double *a, int N);
 
-int main(int argc, char const *argv[]) {
+int main() {
 
   double D = 1;
   double s = -1;
   double t = 0;
   double t_fin = 1.0;
-  int N = atoi(argv[1]);
+  int N;
   double L = 2.0;
   double dx = L/N;
   double dt = (dx*dx)/(2*D);
@@ -27,36 +27,41 @@ int main(int argc, char const *argv[]) {
   double change = 1000;
   int iteraciones = 0;
 
-  cond_iniciales(pasado,N);
-  //escribe(pasado, t, N);
-  while (change > 10E-5) {
+  for (size_t i = 1; i < 11; i++) {
+    N = i*10;
+    change = 1000;
+    iteraciones = 0;
+    cond_iniciales(pasado,N);
 
-    iteraciones ++;
-    dif = 0;
-    // cout << iteraciones << endl;
-    for (size_t i = 0; i < N; i++) {
-      if ((i == 0) | (i == (N-1))){
-        presente[i] = 0.0;
+    while (change > 10E-5) {
+
+      iteraciones ++;
+      dif = 0;
+      // cout << iteraciones << endl;
+      for (size_t i = 0; i < N; i++) {
+        if ((i == 0) | (i == (N-1))){
+          presente[i] = 0.0;
+        }
+        else {
+          presente[i] = pasado[i] + cons * (pasado [i+1] - 2 * pasado [i] + pasado [i-1]) + dt * s;
+        }
+        // cout << pasado[i] << " , " << presente[i] << endl;
+        dif = dif + abs(presente[i]-pasado[i]);
+        //cout << "diff: " << dif << endl;
       }
-      else {
-        presente[i] = pasado[i] + cons * (pasado [i+1] - 2 * pasado [i] + pasado [i-1]) + dt * s;
-      }
-      // cout << pasado[i] << " , " << presente[i] << endl;
-      dif = dif + abs(presente[i]-pasado[i]);
-      //cout << "diff: " << dif << endl;
+      // cout << "dif: " << dif << endl;
+      // cout << "max: " << maximo(presente, N) << endl;
+      change = dif/maximo(presente,N);
+      // cout << "change: " << change << endl;
+      //cout << change << endl;
+      //escribe(presente,t,N);
+      copia(pasado, presente, N);
+
     }
-    // cout << "dif: " << dif << endl;
-    // cout << "max: " << maximo(presente, N) << endl;
-    change = dif/maximo(presente,N);
-    // cout << "change: " << change << endl;
-    //cout << change << endl;
-    //escribe(presente,t,N);
-    copia(pasado, presente, N);
+
+    cout << N << " " << iteraciones << endl;
 
   }
-
-
-  cout << "num de iteraciones: " << iteraciones << endl;
 
 
   delete []pasado;
